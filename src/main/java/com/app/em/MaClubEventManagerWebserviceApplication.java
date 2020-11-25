382a6da3024bb31f8afa5c6f6b8a699929e26707
Tomek Wąsik <wasik.tomasz.wro@gmail.com>
2020-11-25 12:47:26 +0100

    Configure datasource - database connection
    Hibernate configuration properties have been added to
    application.properties file.
    
    Properties like database username and password were placed in
    a separate property file - db-access.properties file which will be
    ignored by Git. It was also necessary to add @PropertySource
    annotation to the main class to let Spring read from this file.
    
    Also, a corresponding dependencies (jpa/jdbc/mysql-connector) have
    been added to a pom.xml file.

--- src/main/java/com/app/em/MaClubEventManagerWebserviceApplication.java
+++ src/main/java/com/app/em/MaClubEventManagerWebserviceApplication.java
@@ -2,8 +2,11 @@ package com.app.em;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
 
 @SpringBootApplication
+@PropertySource("classpath:db-access.properties")
 public class MaClubEventManagerWebserviceApplication {
 
 	public static void main(String[] args) {
