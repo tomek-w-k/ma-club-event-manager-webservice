1ed15577e37890004f7e5ffc88b8ab9d25ff8fb2
Tomek Wąsik <wasik.tomasz.wro@gmail.com>
2020-11-25 12:29:29 +0100

    Initial commit

---
+++ src/main/java/com/app/em/MaClubEventManagerWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.app.em;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaClubEventManagerWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaClubEventManagerWebserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/em/MaClubEventManagerWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.em;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaClubEventManagerWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
