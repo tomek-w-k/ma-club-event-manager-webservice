a8563966510c72df04e2b306a75a0096950d4e83
Tomek Wąsik <wasik.tomasz.wro@gmail.com>
2021-03-17 22:45:07 +0100

    Implement CRUD methods in RankController class and refactor getAllRanks() method

--- src/main/java/com/app/em/controller/user/RankController.java
+++ src/main/java/com/app/em/controller/user/RankController.java
@@ -1,16 +1,16 @@
 package com.app.em.controller.user;
 
 import com.app.em.persistence.entity.user.Rank;
+import com.app.em.persistence.entity.user.User;
 import com.app.em.persistence.repository.user.RankRepository;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.app.em.persistence.repository.user.UserRepository;
+import com.app.em.security.payload.response.MessageResponse;
+import com.app.em.utils.ListToResponseEntityWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,16 +23,69 @@ public class RankController
     RankRepository rankRepository;
 
     @Autowired
-    ObjectMapper objectMapper;
+    UserRepository userRepository;
 
+    @Autowired
+    ListToResponseEntityWrapper listToResponseEntityWrapper;
+
+
+    @PostMapping(value = "/ranks", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity addRank(@RequestBody Rank rank)
+    {
+        return rankRepository.findByRankName(rank.getRankName())
+                .map(this::rankAlreadyExists)
+                .orElseGet(() -> ResponseEntity.ok(rankRepository.save(rank)));
+    }
+
+    @GetMapping(value = "/ranks/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity getRank(@PathVariable Integer id)
+    {
+        return ResponseEntity.of( rankRepository.findById(id) );
+    }
 
     @GetMapping(value = "/ranks", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity getAllRanks() throws JsonProcessingException
+    public ResponseEntity getAllRanks()
     {
-        Optional<List<Rank>> ranksOptional = Optional.ofNullable(rankRepository.findAll());
-        if ( ranksOptional.isEmpty() )
-            return ResponseEntity.notFound().build();
+        return Optional.ofNullable(rankRepository.findAll())
+                .map(listToResponseEntityWrapper::wrapListInResponseEntity)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
 
-        return ResponseEntity.ok(objectMapper.writeValueAsString(ranksOptional.get()));
+    @PutMapping(value = "/ranks", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity updateRank(@RequestBody Rank rank)
+    {
+        return rankRepository.findById(rank.getId())
+                .map(rankToUpdate -> ResponseEntity.ok(rankRepository.save(rank)))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping(value = "/ranks/{id}")
+    public ResponseEntity deleteRank(@PathVariable Integer id)
+    {
+        return rankRepository.findById(id)
+                .map(rank -> {
+                    return Optional.ofNullable(userRepository.findByRank(rank))
+                            .map(this::rankHasUsersAssigned)
+                            .orElseGet(() -> {
+                                rankRepository.delete(rank);
+                                return ResponseEntity.ok().build();
+                            });
+                }).orElseGet(() ->  ResponseEntity.notFound().build());
+    }
+
+    // - - - PRIVATE METHODS - - -
+
+    private ResponseEntity rankAlreadyExists(Rank rank)
+    {
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body(new MessageResponse("A rank " + rank.getRankName() + " already exists."));
+    }
+
+    private ResponseEntity rankHasUsersAssigned(List<User> users)
+    {
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body(new MessageResponse("A rank " +
+                        users.stream().findAny().map(user -> user.getRank().getRankName()) +
+                        " cannot be removed because it has one or more people assigned. Change their rank and try again."));
     }
 }
