87663eede43f3bcf17b75421fe5c610641cb3fef
Tomek Wąsik <wasik.tomasz.wro@gmail.com>
2022-05-29 21:44:00 +0200

    Refactor: Implement constructor injection for ListToResponseEntityWrapper class

--- src/main/java/com/app/em/utils/ListToResponseEntityWrapper.java
+++ src/main/java/com/app/em/utils/ListToResponseEntityWrapper.java
@@ -2,18 +2,22 @@ package com.app.em.utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+
 import java.util.List;
 
 
 @Component
 public class ListToResponseEntityWrapper
 {
-    @Autowired
-    ObjectMapper objectMapper;
+    private final ObjectMapper objectMapper;
 
+
+    public ListToResponseEntityWrapper(ObjectMapper objectMapper)
+    {
+        this.objectMapper = objectMapper;
+    }
 
     public ResponseEntity wrapListInResponseEntity(List<?> items)
     {
